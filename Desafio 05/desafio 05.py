4d6ca4aedbdbd5fd9395687cdff030ebacd403e9
ildaemanoely <ildaemanoely@hotmail.com>
2019-06-25 15:02:05 -0300

    Inserindo o script do desafio 05 jutamente com seu Readme.md

---
+++ Desafio 05/desafio 05.py
@@ -0,0 +1,8 @@
+print('-----------------------')
+print('     Desafio 05')
+print('-----------------------')
+n = int(input('Digite um número:'))
+ant = n -1
+suc = n + 1
+print('')
+print('O número {} informado possui o antecessor {} e o sucessor {}'.format(n,ant,suc))
